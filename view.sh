459fee0926d0b77b9e64b62180f721d3b4802a4d
Alexander Vasin <avasin@accesssoftek.com>
2019-11-20 03:30:16 +0300

    Chaged view script

--- view.sh
+++ view.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-hugo --i18n-warnings server
+DIR=$(pwd)
+PARENTDIR=$(dirname ${DIR})
+NAME=$(basename ${DIR})
+
+RUNNING=$(docker ps -a | grep ${NAME} > /dev/null 2>&1; echo $?)
+
+if [ ${RUNNING} -eq 1 ]; then
+    docker run -ti --name ${NAME} -p 1313:1313 -v ${DIR}:/website -w /website thevasin/myhugo
+else
+    docker start ${NAME}
+    docker attach ${NAME}
+fi
