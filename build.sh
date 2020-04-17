16b84e6a9e25608f1a9873fd2aec14df77a5928a
Alexander Vasin <avasin@accesssoftek.com>
2020-04-18 02:22:15 +0300

    Added live alias

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ NAME=$(basename ${DIR})
 RUNNING=$(docker ps -a | grep ${NAME} > /dev/null 2>&1; echo $?)
 
 docker rm ${NAME}
-docker run -ti --entrypoint="" --name ${NAME} -p 1313:1313 -v ${DIR}:/website -w /website thevasin/myhugo hugo
+docker run -ti --entrypoint="" --name ${NAME} -p 1313:1313 -v ${DIR}:/website -w /website thevasin/myhugo hugo --cleanDestinationDir -d docs
--- content/post/my-live-stream/index.md
+++ content/post/my-live-stream/index.md
@@ -2,7 +2,7 @@
 title: My live stream
 date: 2020-04-17
 aliases:
-    - /live
+    - /live/
 ---
 
 Since we are all sitting at homes anyway I have decided to play guitar live on YouTube and invite some friends over to watch it and share some drinks with me(virtually, via the magic of the internet connection).
